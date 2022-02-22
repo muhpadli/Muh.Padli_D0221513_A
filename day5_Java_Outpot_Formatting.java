ac6a5c61c2e46cbcef36eec3fbc665f2971592e9
muhpadli <muhp5934@gmail.com>
2022-02-23 06:38:27 +0700

    day5_Java_Output_Formatting

--- day5_Java_Outpot_Formatting.java
+++ day5_Java_Outpot_Formatting.java
@@ -6,7 +6,7 @@ public class day5_Java_Outpot_Formatting {
             System.out.println("================================");
             for(int i=0;i<3;i++){
                 String s1=sc.next();
-   int x=sc.nextInt();
+                int x=sc.nextInt();
                 System.out.printf("%-15s%03d%n", s1, x);
             }
             System.out.println("================================");
