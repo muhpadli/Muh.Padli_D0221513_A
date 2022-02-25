75f1374e807ed17c8d6e7689869b302f67523f56
muhpadli <muhp5934@gmail.com>
2022-02-25 21:01:41 +0700

    Laporan Harian

--- day6_Java_Loops_I.java
+++ day6_Java_Loops_I.java
@@ -1,14 +1,10 @@
 package laporan_harian;
 import java.io.*;
 
-
 public class day6_Java_Loops_I {
-    public static void main(String[] args) throws IOException {
+     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(bufferedReader.readLine().trim());
-        for(int j=1; j<=10; j++ ){
-            System.out.println(N+ " x " +j+ " = "+(N*j));
-        }
+        bufferedReader.close();
     }
 }
