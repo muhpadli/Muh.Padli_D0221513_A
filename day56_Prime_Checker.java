fd0b72efb12e43be29b74e8bb11acfc87250990a
palli <96329788+muhpadli@users.noreply.github.com>
2022-04-15 23:15:54 +0800

    day 56

---
+++ day56_Prime_Checker.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laporan_harian;
+
+/**
+ *
+ * @author ASUS
+ */
+public class day56_Prime_Checker {
+    
+}
